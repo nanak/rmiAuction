083466f4d063e55369eeaa89e81dbf774f10149e
Rudolf Krepela <rudolf.krepela@student.tgm.ac.at>
2014-02-11 14:39:29 +0100

    deletepricestep junittest and exception junittests

--- src/JUnitTests/BillingServerSecureTest.java
+++ src/JUnitTests/BillingServerSecureTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import billing.BillingServerSecure;
+import billing.PriceStep;
+import Exceptions.IllegalValueException;
 import Exceptions.PriceStepIntervalOverlapException;
 
 /**
@@ -40,6 +42,20 @@ public class BillingServerSecureTest{
 			s.createPriceStep(0, 10, 5, 10);
 			s.createPriceStep(0, 11, 5, 6);
 	}
+	
+	@Test(expected=PriceStepIntervalOverlapException.class)
+	public void createPriceStepTestPriceStepIntervalOverlapException2() throws PriceStepIntervalOverlapException {
+		BillingServerSecure s= new BillingServerSecure();
+			s.createPriceStep(10, 0, 5, 10);
+			s.createPriceStep(11, 0, 5, 6);
+	}
+	
+	@Test(expected=PriceStepIntervalOverlapException.class)
+	public void createPriceStepTestPriceStepIntervalOverlapException3() throws PriceStepIntervalOverlapException {
+		BillingServerSecure s= new BillingServerSecure();
+			s.createPriceStep(10, 0, 5, 10);
+			s.createPriceStep(11, 100, 5, 6);
+	}
 
 	@Test
 	public void deletePriceStepTest() {
@@ -51,5 +67,16 @@ public class BillingServerSecureTest{
 			assertTrue(true);
 		} catch (PriceStepIntervalOverlapException e) {
 		}
+	}
+	
+	@Test
+	public void toStringTest(){
+		try {
+			PriceStep p= new PriceStep(0, 10, 5, 5);
+			System.out.println(p.toString());
+			assertEquals(p.toString(), "PriceSteps [startPrice=0.0, endPrice=10.0, fixedPrice=5.0, variablePricePercent=5.0]");
+		} catch (IllegalValueException e) {
+		}
+		
 	}
 }
