c39abf17140fb7f01b9a7e0f6a9220a2121be409
Thomas Traxler <ttraxler@student.tgm.ac.at>
2014-02-12 12:49:10 +0100

    actualized UML

--- src/billing/StartBillingServer.java
+++ src/billing/StartBillingServer.java
@@ -1,8 +1,12 @@
 package billing;
 
+import java.io.UnsupportedEncodingException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 /**
@@ -16,7 +20,11 @@ import java.rmi.server.UnicastRemoteObject;
 public class StartBillingServer {
 
 	public static void main(String[] args) {
+		BillingServer bs = new BillingServer (loginTestMap());
+		
 	}
+	
+	
 	
 	/**
 	 * Initialisiert den RMI-stub fuer den Billingserver
@@ -37,5 +45,22 @@ public class StartBillingServer {
 	        }catch (Exception e){
 	        	//TODO Handling
 	        }
+	 }
+	 
+	 private static ConcurrentHashMap<String,byte[]> loginTestMap(){
+		 byte[] bytesOfMessage;
+			MessageDigest md;
+			try {
+				bytesOfMessage = "test".getBytes("UTF-8");
+				md = MessageDigest.getInstance("MD5");
+				byte[] thedigest = md.digest(bytesOfMessage);
+				ConcurrentHashMap<String,byte[]> ret = new ConcurrentHashMap<String,byte[]>();
+				ret.put("test", thedigest);
+				return ret;
+			} catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
 	 }
 }
