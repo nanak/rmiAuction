aafffa54e64ca1ac5ca8d2327fb4df65f6ec978a
Rudolf Krepela <rudolf.krepela@student.tgm.ac.at>
2014-02-12 11:12:26 +0100

    bill commented and print function added

--- src/billing/Bill.java
+++ src/billing/Bill.java
@@ -1,7 +1,16 @@
 package billing;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
+/**
+ * saves the bill history for one user
+ * 
+ * @author Rudolf Krepela
+ * @email rkrepela@student.tgm.ac.at
+ * @version 12.02.2014
+ *
+ */
 public class Bill{
 	
 	private String user;
@@ -19,8 +28,19 @@ public class Bill{
 	@Override
 	public String toString() {
 		String r="Bill for "+user+" ";
+		Iterator<Long> a=auctionID.iterator();
+		Iterator<Double> b=price.iterator();
+		while(b.hasNext()){
+			r=r+"\n"+"ID: "+a.next().toString()+" Price: "+b.next().toString();
+		}
+		return r;
 	}
 
+	/**
+	 * saves a billing line
+	 * @param auctionID
+	 * @param price
+	 */
 	public void addBillingLine(long auctionID, double price){
 		this.auctionID.add(auctionID);
 		this.price.add(price);
