644424723301cbbc17ef7fe5de716ba539675893
Nanak Tattyrek <nanak@tattyrek.at>
2014-02-16 15:51:50 +0100

    added write and delete file

--- src/ServerModel/FileHandler.java
+++ src/ServerModel/FileHandler.java
@@ -1,27 +1,68 @@
 package ServerModel;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class FileHandler<K,T> {
- 
+/**
+ * 
+ * @author Nanak
+ * 
+ * @param <K>
+ * @param <T>
+ */
+public class FileHandler<K, T> {
+
 	private File file;
-	 
+	private String filename;
+
+	public FileHandler() {
+		this.filename = "file.txt";
+	}
+
 	public boolean writeObject(K key, T value) {
+		ObjectOutputStream out;
+		try {
+			file = new File(filename);
+			out = new ObjectOutputStream(new FileOutputStream(file));
+			out.writeObject(value);
+			out.flush();
+			out.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("Error with specified file");
+			ex.printStackTrace();
+			return false;
+		} catch (IOException ex) {
+			System.out.println("Error with I/O processes");
+			ex.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	public boolean writeMap(ConcurrentHashMap<K, T> map) {
 		return false;
 	}
-	 
-	public boolean writeMap(ConcurrentHashMap<K,T> map) {
-		return false;
-	}
-	 
-	public ConcurrentHashMap<K,T> readAll() {
+
+	public ConcurrentHashMap<K, T> readAll() {
 		return null;
 	}
-	 
+
 	public boolean deleteFile() {
-		return false;
+		try{
+		file = new File(filename);
+		if(file.delete()){
+			return true;
+		}
+		else{
+			return false;
+		}
+		}
+		catch(Exception e){
+			return false;
+		}
 	}
-	 
 }
- 
