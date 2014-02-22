45468b4c9b7b42ec9b46298b8335144cbc32a677
Nanak Tattyrek <nanak@tattyrek.at>
2014-02-22 17:09:50 +0100

    filehandler in working condition, test class

--- src/ServerModel/FileHandler.java
+++ src/ServerModel/FileHandler.java
@@ -7,129 +7,121 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 
+import Exceptions.CannotCastToMapException;
+
 /**
  * This class handles the saving to and reading from file.
+ * 
  * @author Nanak Tattyrek
  * 
- * @param <K> Type of the Key to use
- * @param <T> Type of the Value to use
+ * @param <K>
+ *            Type of the Key to use
+ * @param <T>
+ *            Type of the Value to use
  */
-public class FileHandler<K, T> {
+public class FileHandler<K extends Serializable, T extends Serializable> {
 
 	private File file;
 	private String filename;
-	private ObjectInputStream in;
 	private FileInputStream fileIn;
 	private FileOutputStream fileOut;
 
 	/**
-	 * default constructor
-	 */
-	public FileHandler() {
-		this.filename = "file.txt";
-		file = new File(filename);
-	}
-	
-	/**
 	 * constructor, sets the filename to use for the program
-	 * @param filename the filename to use
+	 * 
+	 * @param filename
+	 *            the filename to use
+	 * @throws IOException
+	 *             if any input/output operations fail
 	 */
-	public FileHandler(String filename){
+	public FileHandler(String filename) throws IOException {
 		this.filename = filename;
 		file = new File(filename);
-		if(file.exists()){
-			try {
-				this.in=new ObjectInputStream(new FileInputStream(file));
-				fileIn = new FileInputStream(file);
-				fileOut = new FileOutputStream(file);
-			} catch (FileNotFoundException e) {
-				System.out.println("Error with specified file");
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		fileOut = new FileOutputStream(file);
+		fileIn = new FileInputStream(file);
 	}
-	
+
 	/**
-	 * close ObjectInputStream
+	 * close streams
+	 * 
+	 * @throws IOException
+	 *             if any input/output operations fail
 	 */
-	public void close(){
-		try {
-			in.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public void close() throws IOException {
+		fileIn.close();
+		fileOut.close();
 	}
 
 	/**
 	 * Reads a single Object from file
-	 * @param key the Key to use
-	 * @param value the Value to use
+	 * 
+	 * @param key
+	 *            the Key to use
 	 * @return the read object
+	 * @throws IOException
+	 *             if any input/output operations fail
+	 * @throws CannotCastToMapException
+	 *             if the map can't be casted to ConcurrentHashMap
 	 */
-	public Object readObject(K key, T value) { // TODO: Key, value lesen
-		ConcurrentHashMap<K, T> map = new ConcurrentHashMap<K, T>();
-		Properties prop = new Properties();
+	public Object readObject(K key) throws IOException,
+			CannotCastToMapException {
+		ConcurrentHashMap<K, T> map;
+		ObjectInputStream ois = new ObjectInputStream(fileIn);
 		try {
-			prop.load(new FileInputStream(filename));
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found.");
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.out.println("Error with I/O processes");
-			e.printStackTrace();
+			map = (ConcurrentHashMap<K, T>) ois.readObject();
+		} catch (ClassNotFoundException e) {
+			throw new CannotCastToMapException();
 		}
-		prop.putAll(map);
 		return map.get(key);
 	}
 
 	/**
 	 * Writes a single Object to file
-	 * @param key the Key to use
-	 * @param value the Value to use
+	 * 
+	 * @param key
+	 *            the Key to use
+	 * @param value
+	 *            the Value to use
 	 * @return true if successful, false if unsuccessful
+	 * @throws IOException
+	 *             if any input/output operations fail
+	 * @throws CannotCastToMapException
+	 *             if the map can't be casted to ConcurrentHashMap
 	 */
-	public boolean writeObject(K key, T value) { // TODO: key, value speichern
-		ConcurrentHashMap<K, T> map = new ConcurrentHashMap<K, T>();
-		Properties prop1 = new Properties();
-		Properties prop2 = new Properties();
+	public boolean writeObject(K key, T value) throws IOException,
+			CannotCastToMapException { // TODO: key, value speichern
+		ConcurrentHashMap<K, T> map;
+		FileInputStream fileIn = new FileInputStream(file);
+		ObjectInputStream ois = new ObjectInputStream(fileIn);
+
 		try {
-			prop1.load(new FileInputStream(filename));
-			prop1.putAll(map);
-			map.put(key, value);
-			
-			for (ConcurrentHashMap.Entry<K, T> entry : map.entrySet()) {
-				prop2.put(entry.getKey(), entry.getValue());
-			}
-			prop2.store(new FileOutputStream(filename), null);
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found.");
-			e.printStackTrace();
-			return false;
-		} catch (IOException e) {
-			System.out.println("Error with I/O processes");
-			e.printStackTrace();
+			map = (ConcurrentHashMap<K, T>) ois.readObject();
+		} catch (ClassNotFoundException e) {
 			return false;
 		}
+		map.put(key, value);
+		writeMap(map);
 		return true;
 	}
 
 	/**
 	 * Saves a Map to file
-	 * @param map the map to save
+	 * 
+	 * @param map
+	 *            the map to save
 	 * @return true if successful, false if unsuccessful
+	 * @throws IOException
+	 *             if any input/output operations fail
 	 */
-	public boolean writeMap(ConcurrentHashMap<K, T> map) {
-		Properties prop = new Properties();
-		for (ConcurrentHashMap.Entry<K, T> entry : map.entrySet()) {
-			prop.put(entry.getKey(), entry.getValue());
-		}
+	public boolean writeMap(ConcurrentHashMap<K, T> map) throws IOException {
+		ObjectOutputStream ostream = new ObjectOutputStream(fileOut);
 		try {
-			prop.store(new FileOutputStream(filename), null);
+			ostream.writeObject(map);
+			ostream.flush();
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found.");
 			e.printStackTrace();
@@ -144,37 +136,35 @@ public class FileHandler<K, T> {
 
 	/**
 	 * Reads a Map from file
+	 * 
 	 * @return the ConcurrentHashMap which was read from file
+	 * @throws IOException
+	 *             if any input/output operations fail
+	 * @throws CannotCastToMapException
+	 *             if the map can't be casted to ConcurrentHashMap
 	 */
-	public ConcurrentHashMap<K, T> readAll() {
-		ConcurrentHashMap<K, T> map = new ConcurrentHashMap<K, T>();
-		Properties prop = new Properties();
+	public ConcurrentHashMap<K, T> readAll() throws IOException,
+			CannotCastToMapException {
+		ConcurrentHashMap<K, T> map;
+		ObjectInputStream ois = new ObjectInputStream(fileIn);
 		try {
-			prop.load(new FileInputStream(filename));
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found.");
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.out.println("Error with I/O processes");
-			e.printStackTrace();
+			map = (ConcurrentHashMap<K, T>) ois.readObject();
+		} catch (ClassNotFoundException e) {
+			throw new CannotCastToMapException();
 		}
-		prop.putAll(map);
 		return map;
 	}
 
 	/**
 	 * deletes a file
+	 * 
 	 * @return true if successful, false if unsuccessful
 	 */
 	public boolean deleteFile() {
-		try {
-			file = new File(filename);
-			if (file.delete()) {
-				return true;
-			} else {
-				return false;
-			}
-		} catch (Exception e) {
+		file = new File(filename);
+		if (file.delete()) {
+			return true;
+		} else {
 			return false;
 		}
 	}
---
+++ src/ServerModel/FileHandlerTest.java
@@ -0,0 +1,78 @@
+package ServerModel;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+import Exceptions.CannotCastToMapException;
+
+import model.LoginMessage;
+import model.Message;
+
+/**
+ * simple filehandler test class
+ * @author Nanak Tattyrek
+ *
+ */
+public class FileHandlerTest {
+	private static FileHandler<Integer, Message> fh;
+	
+	public static void main(String[] args) {
+		try {
+			fh = new FileHandler("file.txt");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		ConcurrentHashMap<Integer, Message> hm = new ConcurrentHashMap<>();
+		hm.put(1, new LoginMessage());
+		hm.put(2, new LoginMessage());
+		try {
+			fh.writeMap(hm);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			System.out.println(fh.readAll().toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CannotCastToMapException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			fh.writeObject(3, new LoginMessage());
+			fh.writeObject(1, new LoginMessage());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CannotCastToMapException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			System.out.println(fh.readAll().toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CannotCastToMapException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			System.out.println(fh.readObject(2).toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CannotCastToMapException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(fh.deleteFile());
+	}
+}
