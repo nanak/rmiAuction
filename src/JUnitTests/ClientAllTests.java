dde7744e622506eafa48126876b7abd8fbe6a160
Nanak Tattyrek <nanak@tattyrek.at>
2014-02-23 14:00:38 +0100

    added ClientAllTests

---
+++ src/JUnitTests/ClientAllTests.java
@@ -0,0 +1,12 @@
+package JUnitTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ClientTest.class, TaskExecuterTest.class, CLITest.class})
+public class ClientAllTests {
+
+	
+}
