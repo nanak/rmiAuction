f9d355f5d1243055998a28603e5d43ba29aecfff
Nanak Tattyrek <nanak@tattyrek.at>
2014-02-24 19:29:05 +0100

    javadoc added in client package test classes and filehandler test

--- src/JUnitTests/FileHandlerUnitTest.java
+++ src/JUnitTests/FileHandlerUnitTest.java
@@ -23,79 +23,91 @@ import billing.Bill;
  * @author Nanak Tattyrek, Rudolf Krepela
  * @version 23.02.2014
  * @email ntattyrek@student.tgm.ac.at, rkrepela@student.tgm.ac.at
- *
+ * 
  */
 public class FileHandlerUnitTest {
 	private FileHandler<Integer, Message> fh;
 	private ConcurrentHashMap<Integer, Message> hm;
 
+	/**
+	 * Gets executed before every test method creates a new FileHandler and
+	 * ConcurrentHashMap an filles the map
+	 */
 	@Before
-	public void setUp(){
+	public void setUp() {
 		try {
 			fh = new FileHandler<Integer, Message>("file.txt");
 			hm = new ConcurrentHashMap<Integer, Message>();
 		} catch (IOException e) {
 		}
-		hm.put(1, new LoginMessage("login1","adresse",10,20));
-		hm.put(2, new LoginMessage("login2","dresse",10,20));
+		hm.put(1, new LoginMessage("login1", "adresse", 10, 20));
+		hm.put(2, new LoginMessage("login2", "dresse", 10, 20));
 	}
-	
+
+	/**
+	 * Tests if the readAll() method works as expected
+	 */
 	@Test
-	public void testReadAll(){
+	public void testReadAll() {
 		try {
-			ConcurrentHashMap<Integer, Message> test=(ConcurrentHashMap<Integer, Message>) fh.readAll();
-			assertEquals(test.get(1).getName(),"login1");
-			assertEquals(test.get(2).getName(),"login2");
+			ConcurrentHashMap<Integer, Message> test = (ConcurrentHashMap<Integer, Message>) fh
+					.readAll();
+			assertEquals(test.get(1).getName(), "login1");
+			assertEquals(test.get(2).getName(), "login2");
 			assertNotNull(test);
 		} catch (IOException e) {
 		} catch (CannotCastToMapException e) {
 		}
 	}
-	
+
+	/**
+	 * Tests if the readObject() and writeObject() methods work as expected
+	 */
 	@Test
-	public void testWriteReadObject(){
+	public void testWriteReadObject() {
 		try {
-			assertTrue(fh.writeObject(3, new LoginMessage("login3","adresse",10,20)));
-			assertEquals(((Message)(fh.readObject(3))).getName(),"login3");
-			assertEquals(((Message)(fh.readObject(1))).getName(),"login1");
+			assertTrue(fh.writeObject(3, new LoginMessage("login3", "adresse",
+					10, 20)));
+			assertEquals(((Message) (fh.readObject(3))).getName(), "login3");
+			assertEquals(((Message) (fh.readObject(1))).getName(), "login1");
 		} catch (IOException e) {
 		} catch (CannotCastToMapException e) {
 		}
 	}
-	
+
+	/**
+	 * Tests if the writeMap() method works as expected
+	 */
 	@Test
-	public void testWriteMap(){
+	public void testWriteMap() {
 		try {
 			assertTrue(fh.writeMap(hm));
 		} catch (IOException e) {
 		}
 	}
-	
+
+	/**
+	 * Tests if a single object can be read out of a whole map that was saved
+	 * before
+	 */
 	@Test
-	public void writeReadBillConcurrentHashMap(){
+	public void writeReadBillConcurrentHashMap() {
 		try {
-			FileHandler<String, Bill> fh = new FileHandler<String, Bill>("billtest.txt");
-			Bill bill= new Bill("user",1,10.4,1.2,2);
-			//writeObject fails
-			//assertTrue(fh.writeObject("user", bill));
-			ConcurrentHashMap<String, Bill> map=new ConcurrentHashMap<String,Bill>();
+			FileHandler<String, Bill> fh = new FileHandler<String, Bill>(
+					"billtest.txt");
+			Bill bill = new Bill("user", 1, 10.4, 1.2, 2);
+			// writeObject fails
+			// assertTrue(fh.writeObject("user", bill));
+			ConcurrentHashMap<String, Bill> map = new ConcurrentHashMap<String, Bill>();
 			map.put("user", bill);
 			assertTrue(fh.writeMap(map));
-			assertEquals(((Bill)(fh.readObject("user"))).toString(),"auction_ID	strike_price	fee_fixed	fee_variable	fee_total\n1		10,40		1,20		0,21		1,41		\n");
+			assertEquals(
+					((Bill) (fh.readObject("user"))).toString(),
+					"auction_ID	strike_price	fee_fixed	fee_variable	fee_total\n1		10,40		1,20		0,21		1,41		\n");
 			assertTrue(fh.deleteFile());
 		} catch (IOException | CannotCastToMapException e) {
 			e.printStackTrace();
 		}
 	}
-	
-//	@Test
-//	public void testDeleteFile() {
-//		try {
-//			fh = new FileHandler<Integer, Message>("file.txt");
-//			assertTrue(fh.deleteFile());
-//		} catch (IOException e) {
-//		} catch (CannotCastToMapException e) {
-//		}
-//	}
 
 }
